93e4910bcb0b67646efba719549fd0c57eefaf9f
Vincenius <vincent.will@vaitrade.de>
2024-02-23 18:10:51 +0100

    initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import type { Component } from 'solid-js';
 import { createSignal, createEffect } from "solid-js";
+import "@github/relative-time-element"
 
 import logo from './logo.svg';
 import styles from './App.module.css?inline';
@@ -12,15 +13,19 @@ const App: Component = () => {
   const [feedData, setFeedData] = createSignal([]);
 
   createEffect(() => {
+      // https://docs.bsky.app/docs/api/app-bsky-feed-get-author-feed
       agent.app.bsky.feed.getAuthorFeed({
         limit: 10,
         actor: "vincentwill.com",
+        filter: "posts_no_replies",
       }).then(({ success, data }) => {
         if (success) {
           const feed = (data.feed || []).map(({ post }) => ({
             username: post.author.displayName,
-            profilePicture: post.author.avatar, // todo fallback
+            handle: post.author.handle,
+            picture: post.author.avatar, // todo fallback
             text: post.record.text,
+            createdAt: post.record.createdAt,
             uri: post.uri,
             // todo images
           }));
@@ -37,9 +42,27 @@ const App: Component = () => {
       <style>
         {styles}
       </style>
-      <div class={classes.container}>
-        {feedData().map(post => post.text)}
-      </div>
+      <section>
+        {feedData().map(post => <figure class={classes.post}>
+            <img src={post.picture} alt="profile picture" class={classes.picture} />
+            <blockquote>
+              <span>
+                <a href="#" class={classes.profileLink}>
+                  <span class={classes.username}>{post.username}</span>
+                  <span class={classes.handle}>@{post.handle}</span>
+                </a>
+                ·
+                <span class={classes.createdAt}>
+                  <relative-time datetime={post.createdAt} format="micro" threshold="P30D"></relative-time>
+                </span>
+              </span>
+              <p class={classes.text}>{post.text}</p>
+              {/* todo map linebreaks & links */}
+              {/* todo image & link to post */}
+            </blockquote>
+        </figure>
+        )}
+      </section>
     </>
 
   );
